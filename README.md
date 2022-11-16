dbb8a465a08283b87dc66716e9db883f3f15321f
mozaBit <bamhammedmetehri@gmail.com>
2022-11-16 17:34:47 +0000

    finished essential things

--- README.md
+++ README.md
@@ -2,11 +2,6 @@
 
  Func Name  |                  Prototype              |     Description                       |
 :----------:|:---------------------------------------:|:-------------------------------------:|
- Add        |  Add(a int, b int) int {}               |  returns a + b                        |
- Mul        |  Mul(a int, b int) int {}               |  reurns a * b                         |
- Div        |  Div(a int, b int) int {}               |  returns a / b                        |
- Mod        |  Mod(a int, b int) int {}               |  returns a % b                        |
- Sub        |  Sub(a int, b int) int {}               |  returns a - b                        |
  Pi         |  Pi() float {}                          |  returns Pi      (3.14     ...etc)    |
  Cos        |  Cos(adj float, hyp float) float {}     |  returns adj / hyp                    |
  Sin        |  Sin(opp float, hyp float) float {}     |  returns opp / hyp                    |
--- ft_math.go
+++ ft_math.go
@@ -2,42 +2,31 @@ package math
 
 import "math"
 
-func Add(a int, b int) int {
-	return (a + b)
-}
-func Mul(a int, b int) int {
-	return (a * b)
-}
-func Div(a int, b int) int {
-	return (a / b)
-}
-func Mod(a int, b int) int {
-	return (a % b)
-}
-func Sub(a int, b int) int {
-	return (a - b)
-}
-
 // Angle functions
 func Pi() float32 {
 	return (math.Pi)
 }
+
 func Cos(adj float32, hyp float32) float32 {
 	return (adj / hyp)
 }
+
 func Sin(opp float32, hyp float32) float32 {
 	return (opp / hyp)
 }
+
 func Tan(opp float32, adj float32) float32 {
 	return (opp / adj)
 }
 
-// func Ln(nb float32) float32 {
-// 	return (0)
-// }
-// func Exp(nb float32) float32 {
-// 	return (0)
-// }
+func Ln(nb float64) float64 {
+	// return (((11/60)*float32(Pow(int((nb-1)), 3)) + float32(Pow(int((nb-1)), 2))) / (-0.5 + (3/2)*nb + (3/5)*float32(Pow(int(nb-1), 2)) + ((1 / 20) * float32(Pow(int(nb-1), 3)))))
+	return (math.Log(nb))
+}
+
+func Exp(nb float64) float64 {
+	return (math.Exp(nb))
+}
 
 func Sqrt(nb int) int {
 	var sqrt float32
@@ -61,9 +50,43 @@ func Sqrt(nb int) int {
 	return (int(sqrt))
 }
 
-//	func cbrt(nb int) int {
-//		return (0)
-//	}
+func Cbrt(nb int) int {
+	if nb == 0 {
+		return (0)
+	}
+	if nb < 0 {
+		return (-Cbrt(-nb))
+	}
+
+	var r float32 = float32(nb)
+	var ex int = 0
+
+	for r < 0.125 {
+		r *= 8
+		ex--
+	}
+	for r > 1.0 {
+		r *= 0.125
+		ex++
+	}
+	r = (-0.46946116*r+1.072302)*r + 0.3812513
+
+	for ex < 0 {
+		r *= 0.5
+		ex++
+	}
+	for ex > 0 {
+		r *= 2
+		ex--
+	}
+	r = (2.0/3.0)*r + (1.0/3.0)*float32(nb)/(r*r)
+	r = (2.0/3.0)*r + (1.0/3.0)*float32(nb)/(r*r)
+	r = (2.0/3.0)*r + (1.0/3.0)*float32(nb)/(r*r)
+	r = (2.0/3.0)*r + (1.0/3.0)*float32(nb)/(r*r)
+
+	return (int(r))
+}
+
 func Pow(x int, y int) int {
 	var k int
 	var i int
@@ -82,6 +105,7 @@ func Pow(x int, y int) int {
 	}
 	return (k)
 }
+
 func Fact(nb int) int {
 	var i int
 	if nb < 0 {
@@ -96,6 +120,7 @@ func Fact(nb int) int {
 	}
 	return (nb)
 }
+
 func Abs(nb int) int {
 	if nb < 0 {
 		return (-nb)
@@ -103,20 +128,30 @@ func Abs(nb int) int {
 	return (nb)
 }
 
-//	func Floor(nb float32) int {
-//		if (nb < 0 && ) {
-//			return (int(nb) - 1)
-//		}
-//		return (0)
-//	}
-//
-//	func Ceil(nb float32) int {
-//		return (0)
-//	}
-//
-//	func Trunc(nb float32) int {
-//		return (0)
-//	}
+func Floor(nb float32) int {
+	var nbi int
+
+	nbi = int(nb)
+	if nb < float32(nbi) {
+		return (nbi - 1)
+	}
+	return (nbi)
+}
+
+func Ceil(nb float32) int {
+	var nbi int
+
+	nbi = int(nb)
+	if nb == float32(nbi) {
+		return (nbi)
+	}
+	return (nbi + 1)
+}
+
+func Trunc(nb float32) int {
+	return (int(nb))
+}
+
 func Max(a float32, b float32) float32 {
 	if a >= b {
 		return (a)
